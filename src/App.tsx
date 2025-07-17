976342260c949189250e7272b5cace5ecf3d5aa6
melorin4 <lorinerol257@gmail.com>
2025-07-17 13:35:42 +0300

    Finalise project

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,35 @@
-import {useState} from "react";
-import TodoItem from './components/TodoItem.tsx';
-import {dummydata } from './data/todos';
-import AddTodoForm from "./components/AddTodoForm.tsx";
+import {useEffect, useState} from "react";
+import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
+import TodoSummary from "./components/TodoSummary";
+import {Todo} from "./types/todo"
+import { dummydata } from "./data/todos";
+import useTodos from "./hooks/useTodos";
+
 function App() {
-  const [todos, setTodos] = useState(dummydata);
-
-  function setTodoCompleted(id: number, completed: boolean){
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) => (todo.id === id ? {...todo, completed}: todo))
-  );
-  }
-
+  const {
+    todos,
+    addTodo,
+    setTodoCompleted,
+    deleteTodo,
+    deleteAllCompletedTodos,
+  } = useTodos();
   return (
-    <main className="py-10 h-screen space-y-5">
+    <main className="py-10 h-screen space-y-5 overflow-y-auto">
       <h1 className="font-bold text-3xl text-center">Task Manager</h1>
       <div className='max-w-2xl mx-auto bg-slate-100 rounded-md p-5 space-y-6'>
-        <AddTodoForm />
-        <div className='space-y-2'>
-          {todos.map((todo) => (
-            <TodoItem key={todo.id} todo={todo}
-            onCompletedChange={setTodoCompleted} />
+        <AddTodoForm
+        onSubmit={addTodo} />
+        <TodoList 
+        todos={todos}
+        onCompletedChange={setTodoCompleted}
+        onDelete={deleteTodo}/>
+        
 
-            
-            ))}
-        </div>
       </div>
+      <TodoSummary
+      todos={todos}
+      deleteAllCompleted={deleteAllCompletedTodos}/>
     </main>
   )
 }
--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -1,10 +1,35 @@
-export default function AddTodoForm(){
+import { useState } from "react"
+
+interface AddTodoFormProps {
+    onSubmit: (title: string) => void;
+}
+
+export default function AddTodoForm({onSubmit}: AddTodoFormProps){
+    const [input, setInput] = useState("")
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>){
+            e.preventDefault();
+
+            if(!input.trim()) return;
+
+            onSubmit(input)
+            setInput("");
+            
+
+    }
     return(
-        <form>
+        <form className="flex" onSubmit={handleSubmit}>
             <input
+            value={input}
+           onChange={(e) => setInput(e.target.value)}
             placeholder="What do you want to do?"
-            className="rounded-s-md"
+            className="border rounded-s-md grow border-gray-400 p-2"
             />
+            <button
+            type="submit"
+            className="w-16 rounded-e-md bg-slate-900 text-white hover:bg-slate-800">
+                Add
+            </button>
         </form>
     )
 }
